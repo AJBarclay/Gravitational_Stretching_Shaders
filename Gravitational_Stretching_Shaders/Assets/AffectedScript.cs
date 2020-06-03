77155b4801c8db123a1fbd468cbd9a87f925da78
AJBarclay <50785262+AJBarclay@users.noreply.github.com>
2020-06-03 14:34:04 -0700

    Bodged Blackhole Absorbsion

--- Gravitational_Stretching_Shaders/Assets/AffectedScript.cs
+++ Gravitational_Stretching_Shaders/Assets/AffectedScript.cs
@@ -34,16 +34,24 @@ public class AffectedScript : MonoBehaviour
 		a = Mathf.Sqrt(Mathf.Pow(a,2) + Mathf.Pow(z,2));
 		return a;
 	}
+
+    float distance(Vector3 to, Vector3 from)
+    {
+        float toReturn = 0.0f;
+
+        toReturn = Mathf.Sqrt(Mathf.Pow((to.x - from.x), 2) + Mathf.Pow((to.y - from.y), 2) + Mathf.Pow((to.z - from.z), 2));
+
+        return toReturn;
+    }
 	
     // Update is called once per frame
     void Update()
     {
 
-    //Shader.SetGlobalVector("_Vector",Source.transform.position);
-    //GetComponent<Renderer>().material.SetVector("_Vector", new Vector4(Source.transform.position.x, Source.transform.position.y, Source.transform.position.z,1.0f));
 
-    float xPos, yPos, zPos, t;
+    float xPos, yPos, zPos, t, tolerance;
 		t = Time.time/300;
+        tolerance = 0.1f;
 		Vector3 diffPos = (Source.transform.position - transform.position);
 		float absDiff = absDist(diffPos.x,diffPos.y,diffPos.z);
 		xPos = gForce(diffPos.x)*direction(diffPos.x);
@@ -52,8 +60,7 @@ public class AffectedScript : MonoBehaviour
 		Debug.Log("Difference Position: "+diffPos);
 		Acceleration = new Vector3(xPos,yPos,zPos);
 		Debug.Log("Acc:"+Acceleration);
-		Velocity = Velocity +
-			new Vector3(Acceleration.x*t,Acceleration.y*t,Acceleration.z*t);
+		Velocity = Velocity + new Vector3(Acceleration.x*t,Acceleration.y*t,Acceleration.z*t);
 		if (Velocity.x > Cap) Velocity.x = Cap;
 		if (Velocity.y > Cap) Velocity.y = Cap;
 		if (Velocity.z > Cap) Velocity.z = Cap;
@@ -62,9 +69,16 @@ public class AffectedScript : MonoBehaviour
 		if (Velocity.z < -Cap) Velocity.z = -Cap;
 		
 		Debug.Log("Velocity: "+Velocity);
-		transform.position = transform.position 
-			+ new Vector3(Velocity.x*t,Velocity.y*t,Velocity.z*t);
-    GetComponent<Renderer>().sharedMaterial.SetVector("_Vector", Source.transform.position);
+        Vector3 oldTransform = transform.position;
+		transform.position = transform.position	+ new Vector3(Velocity.x*t,Velocity.y*t,Velocity.z*t);
+        float oldDistance = distance(Source.transform.position, oldTransform);
+        float newDistance = distance(Source.transform.position, transform.position);
+        if (newDistance - oldDistance > tolerance)
+        {
+            transform.position = Source.transform.position;
+            transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        }
+        GetComponent<Renderer>().sharedMaterial.SetVector("_Vector", Source.transform.position);
         //transform.position = Source.transform.position + new Vector3(xPos,yPos,zPos);
     }
 }
