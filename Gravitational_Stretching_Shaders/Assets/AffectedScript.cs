b73e8dc44a4c49936ad31a8fb5005c4db0878242
AJBarclay <50785262+AJBarclay@users.noreply.github.com>
2020-06-02 18:15:33 -0700

    Fixed a line ending

---
+++ Gravitational_Stretching_Shaders/Assets/AffectedScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class AffectedScript : MonoBehaviour
+{
+    public GameObject Source;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Source.transform.position +
+        new Vector3(((float)Math.Sin(Time.time))/100.0f, 0,  ((float)-Math.Cos(Time.time))/100.0f);
+    }
+}
