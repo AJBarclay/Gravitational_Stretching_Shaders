76a944a2e8b6f32815132e83093932b1289104fc
AJBarclay <50785262+AJBarclay@users.noreply.github.com>
2020-06-07 19:29:06 -0700

    Added Buttons to Spawn Shuttles and Astronauts

---
+++ Gravitational_Stretching_Shaders/Assets/SpawnObjectScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjectScript : MonoBehaviour
+{
+    public GameObject sampleObject;
+
+    public void AddObject()
+    {
+        float spawnXpos = Random.Range(-30.0f, 30.0f);
+        float spawnZpos = Random.Range(-30.0f, 30.0f);
+        Vector3 spawnPos = new Vector3(spawnXpos, 0.0f, spawnZpos);
+        Instantiate(sampleObject, spawnPos, Quaternion.identity);
+        this.sampleObject.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+    }
+    
+}
---
+++ Gravitational_Stretching_Shaders/Assets/SpawnShuttleScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnShuttleScript : MonoBehaviour
+{
+    public GameObject shuttleObject;
+
+    public void AddObject()
+    {
+        float spawnXpos = Random.Range(-30.0f, 30.0f);
+        float spawnZpos = Random.Range(-30.0f, 30.0f);
+        Vector3 spawnPos = new Vector3(spawnXpos, 0.0f, spawnZpos);
+        Instantiate(shuttleObject, spawnPos, Quaternion.identity);
+        this.shuttleObject.transform.localScale = new Vector3(2.0f, 2.0f, 2.0f);
+    }
+
+}
